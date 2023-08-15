d4f54190dce8fe49016880c622beea53e0bbef32
Idalia Aranda <nidaliaamtz@hotmail.com>
2023-08-15 16:37:04 -0500

    First Commit with this new Api-Hotel

---
+++ src/main/java/com/codellege/apihotel/ApihotelApplication.java
@@ -0,0 +1,13 @@
+package com.codellege.apihotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codellege/apihotel/ApihotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codellege.apihotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
